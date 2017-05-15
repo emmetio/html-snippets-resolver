4536ae0d8ed05e795a2c0369c044e8682651bb03
Sergey Chikuyonok <serge.che@gmail.com>
2017-05-16 02:43:19 +0300

    Updated builder

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,8 @@
 export default {
+	entry: './index.js',
 	external: ['@emmetio/abbreviation'],
+	targets: [
+		{format: 'cjs', dest: 'dist/html-snippets-resolver.cjs.js'},
+		{format: 'es',  dest: 'dist/html-snippets-resolver.es.js'}
+	]
 };
